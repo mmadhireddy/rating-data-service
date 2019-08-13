9370991a4bf9d24cf5a4038c9324558114b60368
Madhu Reddy <madhireddymadhu534@gmail.com>
2019-08-13 15:00:11 -0700

    Rating Data Microservices

---
+++ src/main/java/com/itsmadhu/ratingdataservice/models/UserRatings.java
@@ -0,0 +1,15 @@
+package com.itsmadhu.ratingdataservice.models;
+
+import java.util.List;
+
+public class UserRatings {
+    private List<Rating> ratings;
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+}
---
+++ src/main/java/com/itsmadhu/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,32 @@
+package com.itsmadhu.ratingdataservice.resources;
+
+import com.itsmadhu.ratingdataservice.models.Rating;
+import com.itsmadhu.ratingdataservice.models.UserRatings;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingsResource {
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String moiveId){
+        return new Rating(moiveId,4);
+    }
+    @RequestMapping("/users/{userId}")
+    public UserRatings getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings =  Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3),
+                new Rating("9874",5),
+                new Rating("0011",2)
+        );
+        UserRatings userRatings = new UserRatings();
+        userRatings.setRatings(ratings);
+        return userRatings;
+    }
+}
